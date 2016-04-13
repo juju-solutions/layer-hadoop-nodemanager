c7cc4fe26b80b5febc4d3b0a7e15fb7f2528cbfc
Konstantin Boudnik <cos@boudnik.org>
2016-04-13 09:30:09 -0600

    Cleaning up; needs testing

--- reactive/nodemanager.py
+++ reactive/nodemanager.py
@@ -1,5 +1,5 @@
 from charms.reactive import when, when_not, set_state, remove_state
-from charms.layer.hadoop_base import get_hadoop_base
+from charms.layer.apache_bigtop_base import get_bigtop_base
 from jujubigdata.handlers import YARN
 from jujubigdata import utils
 
@@ -7,24 +7,20 @@ from jujubigdata import utils
 @when('resourcemanager.ready')
 @when_not('nodemanager.started')
 def start_nodemanager(resourcemanager):
-    hadoop = get_hadoop_base()
-    yarn = YARN(hadoop)
-    yarn.configure_nodemanager(
-        resourcemanager.resourcemanagers()[0], resourcemanager.port(),
-        resourcemanager.hs_http(), resourcemanager.hs_ipc())
+    bigtop = get_bigtop_base()
+    yarn = YARN(bigtop)
     utils.install_ssh_key('yarn', resourcemanager.ssh_key())
     utils.update_kv_hosts(resourcemanager.hosts_map())
     utils.manage_etc_hosts()
-    yarn.start_nodemanager()
-    hadoop.open_ports('nodemanager')
+    # TODO add direct invocation to the init.d script
+    bigtop.open_ports('nodemanager')
     set_state('nodemanager.started')
 
 
 @when('nodemanager.started')
 @when_not('resourcemanager.ready')
 def stop_nodemanager():
-    hadoop = get_hadoop_base()
-    yarn = YARN(hadoop)
-    yarn.stop_nodemanager()
-    hadoop.close_ports('nodemanager')
+    bigtop = get_bigtop_base()
+    # TODO add direct invocation to the init.d script
+    bigtop.close_ports('nodemanager')
     remove_state('nodemanager.started')

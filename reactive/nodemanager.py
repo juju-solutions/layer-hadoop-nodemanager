7c69247089f1c32aeca2d165a55ea136c980e118
Konstantin Boudnik <cos@boudnik.org>
2016-04-14 16:23:37 -0600

    Fixing nodemanager to use mapred-slave interface relations to propagate RM hostname

--- reactive/nodemanager.py
+++ reactive/nodemanager.py
@@ -1,26 +1,44 @@
 from charms.reactive import when, when_not, set_state, remove_state
-from charms.layer.apache_bigtop_base import get_bigtop_base
-from jujubigdata.handlers import YARN
-from jujubigdata import utils
+from charms.layer.apache_bigtop_base import get_bigtop_base, get_layer_opts
+from charmhelpers.core import host, hookenv
 
 
-@when('resourcemanager.ready')
+@when_not('resourcemanager.joined')
+def blocked():
+    hookenv.status_set('blocked', 'waiting for resourcemanager relation')
+
+
+@when('resourcemanager.joined')
+@when_not('nodemanager.installed')
+def install_hadoop(resourcemanager):
+    '''Install only if the resourcemanager has sent its FQDN.'''
+    if resourcemanager.resourcemanagers():
+        hookenv.status_set('maintenance', 'installing nodemanager')
+        hostname = resourcemanager.resourcemanagers()[0]
+        bigtop = get_bigtop_base()
+        bigtop.install(NN=hostname)
+        set_state('nodemanager.installed')
+        hookenv.status_set('maintenance', 'nodemanager installed')
+    else:
+        hookenv.status_set('waiting', 'waiting for nodemanager to become ready')
+
+
+@when('nodemanager.installed')
 @when_not('nodemanager.started')
-def start_nodemanager(resourcemanager):
-    bigtop = get_bigtop_base()
-    yarn = YARN(bigtop)
-    utils.install_ssh_key('yarn', resourcemanager.ssh_key())
-    utils.update_kv_hosts(resourcemanager.hosts_map())
-    utils.manage_etc_hosts()
-    # TODO add direct invocation to the init.d script
-    bigtop.open_ports('nodemanager')
+def start_nodemanager():
+    hookenv.status_set('maintenance', 'starting nodemanager')
+    host.service_start('hadoop-yarn-nodemanager')
+    for port in get_layer_opts().exposed_ports('nodemanager'):
+        hookenv.open_port(port)
     set_state('nodemanager.started')
+    hookenv.status_set('active', 'ready')
 
 
 @when('nodemanager.started')
-@when_not('resourcemanager.ready')
+@when_not('resourcemanager.joined')
 def stop_nodemanager():
-    bigtop = get_bigtop_base()
-    # TODO add direct invocation to the init.d script
-    bigtop.close_ports('nodemanager')
+    hookenv.status_set('maintenance', 'stopping nodemanager')
+    for port in get_layer_opts().exposed_ports('nodemanager'):
+    host.service_stop('hadoop-yarn-nodemanager')
     remove_state('nodemanager.started')
+    hookenv.status_set('maintenance', 'nodemanager stopped')

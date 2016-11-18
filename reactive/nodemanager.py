b4100fb0945543c280051a61fc0135703549b630
Kevin W Monroe <kevin.monroe@canonical.com>
2016-11-18 04:26:11 +0000

    ensure service is up in start method; log/status updates if service fails to start or stop

--- reactive/nodemanager.py
+++ reactive/nodemanager.py
@@ -61,23 +61,40 @@ def send_nm_spec(namenode, resourcemanager):
 @when_not('apache-bigtop-nodemanager.started')
 def start_nodemanager(namenode, resourcemanager):
     hookenv.status_set('maintenance', 'starting nodemanager')
-    # NB: service should be started by install, but this may be handy in case
-    # we have something that removes the .started state in the future. Also
-    # note we restart here in case we modify conf between install and now.
-    host.service_restart('hadoop-yarn-nodemanager')
-    for port in get_layer_opts().exposed_ports('nodemanager'):
-        hookenv.open_port(port)
-    set_state('apache-bigtop-nodemanager.started')
-    hookenv.application_version_set(get_hadoop_version())
-    hookenv.status_set('maintenance', 'nodemanager started')
+    # NB: service should be started by install, but we want to verify it is
+    # running before we set the .started state and open ports. We always
+    # restart here, which may seem heavy-handed. However, restart works
+    # whether the service is currently started or stopped. It also ensures the
+    # service is using the most current config.
+    started = host.service_restart('hadoop-yarn-nodemanager')
+    if started:
+        for port in get_layer_opts().exposed_ports('nodemanager'):
+            hookenv.open_port(port)
+        set_state('apache-bigtop-nodemanager.started')
+        hookenv.status_set('maintenance', 'nodemanager started')
+        hookenv.application_version_set(get_hadoop_version())
+    else:
+        hookenv.log('NodeManager failed to start')
+        hookenv.status_set('blocked', 'nodemanager failed to start')
+        remove_state('apache-bigtop-nodemanager.started')
+        for port in get_layer_opts().exposed_ports('nodemanager'):
+            hookenv.close_port(port)
 
 
 @when('apache-bigtop-nodemanager.started')
 @when_not('resourcemanager.ready')
 def stop_nodemanager():
     hookenv.status_set('maintenance', 'stopping nodemanager')
+    stopped = host.service_stop('hadoop-yarn-nodemanager')
+    if stopped:
+        hookenv.status_set('maintenance', 'nodemanager stopped')
+    else:
+        hookenv.log('NodeManager failed to stop')
+        hookenv.status_set('blocked', 'nodemanager failed to stop')
+
+    # Even if the service failed to stop, we want to treat it as stopped so
+    # other apps do not attempt to interact with it. Remove .started and
+    # close our ports.
+    remove_state('apache-bigtop-nodemanager.started')
     for port in get_layer_opts().exposed_ports('nodemanager'):
         hookenv.close_port(port)
-    host.service_stop('hadoop-yarn-nodemanager')
-    remove_state('apache-bigtop-nodemanager.started')
-    hookenv.status_set('maintenance', 'nodemanager stopped')
